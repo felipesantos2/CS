29536d56a6afebfeabdbc0b709f6487ef2018e5d
felipesantos2 <santospinheiro627@gmail.com>
2026-01-23 10:00:44 -0300

    wip

--- 01_memory_arch/c_stack_overflow/stack_overflow.c
+++ 01_memory_arch/c_stack_overflow/stack_overflow.c
@@ -4,15 +4,22 @@
 // #include <header.h>
 
 // #include <std.io>
-void main() {
+int main() {
     // int num  = 10;
     int num[10000000];
     // printf("Olá, mundo! %d", num);
-    
+
     // printf("\n");
-    
-    for (long i = 0; i <= sizeof(num); i++) {
+
+    long size = sizeof(num);
+
+    for (long i = 0; i <= size; i++) {
         num[i] = i+1;
         printf("Olá, mundo! %d", num[i]);
     }
+
+    return 0;
 }
+
+
+// output: Segmentation fault (core dumped)
