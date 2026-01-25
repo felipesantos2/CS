337a554e62e11a20792fb7d49bc9ae8830b76de8
felipesantos2 <santospinheiro627@gmail.com>
2026-01-24 23:18:51 -0300

    wip

--- 01_memory_arch/c_stack_overflow/constants.c
+++ 01_memory_arch/c_stack_overflow/constants.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// #define EITA 0
+#define EITA 0 // pre-processamento
+
+const int eita1 = 6;
+
 int main() {
 
     char f[] = {'f', 't', '5'};
@@ -15,8 +20,13 @@ int main() {
 
     printf("%s \n", __PRETTY_FUNCTION__);
     
-    //convert strin to long int
     printf("%ld \n", atol("11nggg"));
+    
+    printf("---------------------------------------------\n");
+    printf("#define: %d \n", EITA);
+    
+    printf("const:   %d \n", eita1);
+    //convert strin to long int
 
 
     return EXIT_SUCCESS; // EXIST_FAILURE
